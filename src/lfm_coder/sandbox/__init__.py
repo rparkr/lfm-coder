457e702057eddc4560ca59ee0be2dbb1f4cc4902
Ryan Parker <ryanparker.ml@gmail.com>
2026-04-21 05:43:08 -0500

    fix(sandbox): Remove PooledDockerSandbox
    See previous commit message for explanation.

--- src/lfm_coder/sandbox/__init__.py
+++ src/lfm_coder/sandbox/__init__.py
@@ -1,6 +1,5 @@
 from lfm_coder.sandbox.docker_sandbox import DockerSandbox
 from lfm_coder.sandbox.monty_sandbox import MontySandbox
-from lfm_coder.sandbox.pooled_docker_sandbox import PooledDockerSandbox
 from lfm_coder.sandbox.sandbox import Sandbox
 from lfm_coder.sandbox.types import (
     SandboxConfig,
@@ -17,7 +16,6 @@ from lfm_coder.sandbox.types import (
 
 __all__ = [
     "DockerSandbox",
-    "PooledDockerSandbox",
     "MontySandbox",
     "Sandbox",
     "SandboxConfig",
